e91577c6c90065b7684fab18c264d6b7d4d871e9
Nils Olav Handegard <nils.olav.handegard@imr.no>
2024-02-14 09:16:14 +0100

    Added README and setup file.

---
+++ README.md
@@ -0,0 +1,10 @@
+lssstools is a small collection of utilities/library functions to read
+exports from LSSS
+
+# Export file types
+
+## sv(f)
+
+## sv
+
+## Ts(f)
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ektools',
+      version='0.1',
+      description='Utilities to read exports from LSSS',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/CRIMAC-WP4-Machine-learning/lssstools',
+      author='Nils Olav Handegard',
+      author_email='nilsolav@hi.no',
+      license='MIT',
+      packages=['lssstools'],
+      scripts=[],
+      install_requires=['json', 'pandas'],
+      zip_safe=False)
